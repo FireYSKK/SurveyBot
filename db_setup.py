747eeeb66091f058f803921740065104dd9a01cf
FireYSKK <103999993+FireYSKK@users.noreply.github.com>
2022-05-12 22:09:44 +0300

    Add files via upload

--- db_setup.py
+++ db_setup.py
@@ -5,16 +5,17 @@ cursor = sqlite_connection.cursor()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS users(
    userid INT PRIMARY KEY,
+   telegramid INT,
    firstname TEXT,
    lastname TEXT,
-   username TEXT,
    age INT);
 """)
 sqlite_connection.commit()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS tests(
-   testid INT PRIMARY KEY,
-   deadline TEXT);
+cursor.execute("""CREATE TABLE IF NOT EXISTS surveys(
+   surveyid INT PRIMARY KEY,
+   startdate TEXT,
+   enddate TEXT);
 """)
 sqlite_connection.commit()
 
@@ -25,7 +26,6 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS questions(
    answer2 TEXT,
    answer3 TEXT,
    answer4 TEXT,
-   correct INT,
    testid INT,
    FOREIGN KEY(testid) REFERENCES tests(testid));
 """)
@@ -33,10 +33,20 @@ sqlite_connection.commit()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS results(
    resultid INT PRIMARY KEY,
-   score INT,
+   datecompleted TEXT,
    userid INT,
    testid INT,
    FOREIGN KEY(userid) REFERENCES users(userid),
    FOREIGN KEY(testid) REFERENCES tests(testid));
+""")
+sqlite_connection.commit()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS answers(
+   answerid INT PRIMARY KEY,
+   questionid INT,
+   userid INT,
+   option INT,
+   FOREIGN KEY(questionid) REFERENCES questions(questionid),
+   FOREIGN KEY(userid) REFERENCES users(userid));
 """)
 sqlite_connection.commit()
