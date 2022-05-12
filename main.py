4e9ca50f3bd2ad3f28bf95870a9cde2008b88080
FireYSKK <103999993+FireYSKK@users.noreply.github.com>
2022-05-13 01:31:05 +0300

    Add files via upload
    Bot code is here. Working up to menu part

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import sqlite3
 import os.path
 
@@ -10,9 +11,10 @@ cursor = db_connection.cursor()
 # bot = telebot.TeleBot('5191014419:AAEVfQQbMc5SzkTQvghRkvMb9tjUnn5IpNc')
 bot = telebot.TeleBot('5329387829:AAHdtHf3uf1J5lH8mTALsnm6NQznRUUYnOM')
 
+
+next_user_id = 1
 quest = ''
 ans = []
-uid = 0
 check = 0
 adm_ans = 0
 user_ans = 0
@@ -22,45 +24,70 @@ all_users = []
 admin = 431846556
 
 
-def table_val(userid: int, firstname: str, lastname: str, username: str, age: int):
-    cursor.execute('INSERT INTO users VALUES (?, ?, ?, ?, ?)', (userid, firstname, lastname, username, age))
+def table_val(userid: int, telegramid: str, firstname: str, lastname: str, age: int):
+    cursor.execute('INSERT INTO users VALUES (?, ?, ?, ?, ?)', (userid, telegramid, firstname, lastname, age))
     db_connection.commit()
+
+def register_check(message):
+    cursor.execute("SELECT * FROM users WHERE telegramid = ?", (message.from_user.id,))
+    return cursor.fetchall()
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, message.from_user.username)
-    cursor.execute("SELECT * FROM users WHERE username = ?", message.from_user.username)
-    users_found = cursor.fetchall()
-    print(users_found)
-    if not users_found:
-        bot.send_message(message.chat.id,
-                         f'<b>Приветствую! {message.from_user.first_name} {message.from_user.last_name}</b>',
-                         parse_mode='html')
-        bot.send_message(message.chat.id, "Укажите ваш возраст:")
+    bot.send_message(message.chat.id,
+                    f'<b>Приветствую! {message.from_user.first_name}</b>',
+                    parse_mode='html')
+    bot.send_message(message.chat.id,
+                     """Данный бот предназначен для создания опросов и их прохождения пользователями
+Доступный функционал: 
+1. Регистрация в системе
+Планы:
+1. Создание опросов всесильной админской рукой
+2. Главное меню бота
+3. Возможность нелинейного прохождения существующих опросов
+4. Сохранение результатов пользователей
+5. Сбор общей статистики на основе пользовательских данных""")
+    if not register_check(message):
+        bot.send_message(message.chat.id, "Прежде чем начать, пожалуйста, укажите ваш возраст:")
         bot.register_next_step_handler(message, get_age)
     else:
-        bot.send_message(message.chat.id, "Вы уже зарегистрированы")
+        bot.register_next_step_handler(message, menu)
+
+
+@bot.message_handler(commands=['menu'])
+def menu(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    # Запилить выбор только непройденных тестов ТУТ
+    cursor.execute("SELECT surveyid FROM surveys")
+    for i in range (1, cursor.rowcount + 3):
+        markup.add(types.KeyboardButton('Poll_' + str(i)))
+    bot.send_message(message.chat.id, 'Выберите опрос из предложенных', reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
+# Переброс в меню при рандомном вводе
+def random_text_received(message):
+    bot.send_message(message.chat.id, "Отправляю Вас в меню")
+    bot.register_next_step_handler(message, menu)
+
+
 def get_age(message):
+    global next_user_id
     age = 0
     if age == 0:
         try:
-             age = int(message.text)
-        # except age < 0:
-        #      bot.send_message(message.from_user.id, 'О, так Вы из будущего!\nНо давайте без шуток');
-        #      bot.register_next_step_handler(message, get_age);
-        except Exception:
-             bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
-             bot.register_next_step_handler(message, get_age)
-    us_id = message.from_user.id
-    us_name = message.from_user.first_name
-    us_sname = message.from_user.last_name
-    username = message.from_user.username
-    table_val(userid=us_id, firstname=us_name, lastname=us_sname, username=username, age=age)
-    bot.send_message(message.from_user.id, 'Регистрация прошла успешно')
+            age = int(message.text)
+            table_val(userid=next_user_id,
+                      telegramid=message.from_user.id,
+                      firstname=message.from_user.first_name,
+                      lastname=message.from_user.last_name,
+                      age=age)
+            bot.send_message(message.from_user.id, 'Регистрация прошла успешно')
+        except ValueError:
+            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+            bot.register_next_step_handler(message, get_age)
+    bot.register_next_step_handler(message, menu)
 
 
 ###########
@@ -106,8 +133,8 @@ def get_quest(message):
 def get_ans(message):
     global ans
     global adm_ans
-    adm_ans=message.text
-    if adm_ans=='стоп':
+    adm_ans = message.text
+    if adm_ans == 'стоп':
         bot.send_message(admin, 'Вы хотите разослать опрос?')
         bot.register_next_step_handler(message, send_quest)
     else:
@@ -117,12 +144,12 @@ def get_ans(message):
 
 def send_quest(message):
     global check
-    k=1
+    k = 1
     if message.text == 'Да' or message.text == 'да':
         bot.send_message(message.chat.id, quest)
         for i in ans:
-           bot.send_message(message.chat.id, str(k)+'. '+str(i))
-           k += 1
+            bot.send_message(message.chat.id, str(k) + '. ' + str(i))
+            k += 1
         check = 1
 
 
@@ -130,7 +157,7 @@ def answer(message):
     db_connection.execute("SELECT * FROM questions;")
     questions = cursor.fetchall()
     bot.send_message(message.from_user.id, questions[count_quest][1])
-    for i in range(10+2):
+    for i in range(10 + 2):
         if questions[count_quest][i] != "NULL":
             bot.send_message(message.from_user.id, questions[count_quest][i])
         else:
